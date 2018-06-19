7ccd0afd917b8184a711ce5847f174eb38fe7c39
Peilin Duan <pduan@taser.com>
2018-06-18 23:34:29 -0700

    add head click

--- app/src/main/java/com/hfad/zhongyi/TopLevelActivity.java
+++ app/src/main/java/com/hfad/zhongyi/TopLevelActivity.java
@@ -1,15 +1,20 @@
 package com.hfad.zhongyi;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class TopLevelActivity extends Activity {
+public class TopLevelActivity extends AppCompatActivity {
 
     private boolean front = true;
+    Bitmap body;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +24,18 @@ public class TopLevelActivity extends Activity {
 
     public void onClickFlip(View view){
         ImageView imageView = (ImageView)findViewById(R.id.bodyview);
-        if(front){
+        if(front) {
             imageView.setImageResource(R.drawable.cappuccino);
+            findViewById(R.id.headText).setVisibility(View.INVISIBLE);
+        } else {
+            imageView.setImageResource(R.drawable.body);
+            findViewById(R.id.headText).setVisibility(View.VISIBLE);
         }
-        else imageView.setImageResource(R.drawable.body);
         front = !front;
+    }
+
+    public void onClickHead(View view) {
+        Intent intent = new Intent(this, HeadSpecificActivity.class);
+        startActivity(intent);
     }
 }
