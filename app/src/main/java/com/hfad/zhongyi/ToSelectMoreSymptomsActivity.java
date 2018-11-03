df2dc6bbd1ac2e8c103f13f69c0aa34ffd830c0a
Xi Zhou <xizhou175@gmail.com>
2018-11-03 10:39:24 -0400

    save

---
+++ app/src/main/java/com/hfad/zhongyi/ToSelectMoreSymptomsActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.zhongyi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToSelectMoreSymptomsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_select_more_symptoms);
+    }
+}
