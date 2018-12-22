18c0b38751aebd62fccccbe87999bf6aed63b1a8
Peilin Duan <pduan@taser.com>
2018-12-21 23:50:32 -0800

    change server_url

--- app/src/main/java/com/hfad/zhongyi/Config.java
+++ app/src/main/java/com/hfad/zhongyi/Config.java
@@ -5,8 +5,8 @@ public class Config {
     public String server_url;
     private Config() {
         // use "http://10.0.2.2:8080/registration" for emulator
-        server_url = "http://10.0.0.9:8080";
-        // server_url = "http://3.16.147.178:8080"; // ec2 server
+        // server_url = "http://10.0.0.9:8080";
+        server_url = "http://3.16.147.178:8080"; // ec2 server
     }
     public static Config getConfig() {
         if (config == null) {
